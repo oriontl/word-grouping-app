0e2098f3a6ac6c8bde7f7c92e23e03e573565764
Orion Xiao <oxiao@triplelift.com>
2020-07-10 13:30:20 -0400

    Updates

---
+++ precache-manifest.87ab6e78995ef623f413e38db3c7c705.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ff3e7a2f2c3fe6ec8add58d06665196",
+    "url": "/word-grouping-app/index.html"
+  },
+  {
+    "revision": "ef3d011de82a59168602",
+    "url": "/word-grouping-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ef3d011de82a59168602",
+    "url": "/word-grouping-app/static/js/2.ed535318.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/word-grouping-app/static/js/2.ed535318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0b05faf8470790e7479",
+    "url": "/word-grouping-app/static/js/main.235e1535.chunk.js"
+  },
+  {
+    "revision": "7b2e1b5f260434563c71",
+    "url": "/word-grouping-app/static/js/runtime-main.e4ea7c3c.js"
+  }
+]);
