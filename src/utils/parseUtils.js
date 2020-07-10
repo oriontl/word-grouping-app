62ba5c023cddcbcf8611116ae46468b713d1343e
Orion Xiao <oxiao@triplelift.com>
2020-07-10 11:25:06 -0400

    make file reading async

--- src/utils/parseUtils.js
+++ src/utils/parseUtils.js
@@ -1,17 +1,22 @@
 export const parseCSV = (f) => {
   let r = new FileReader();
+  return new Promise((resolve, reject) => {
+    r.onerror = () => {
+      r.abort();
+      reject(new DOMException("Error loading file."));
+    };
 
-  r.onload = () => {
-    const strings = r.result.split('\n');
-    console.log(strings);
-    const groups = groupByPrefix(strings);
-    console.log(groups);
-  };
+    r.onload = () => {
+      const strings = r.result.split('\n');
+      const groups = groupByPrefix(strings);
+      resolve(groups);
+    };
 
-  r.readAsBinaryString(f);
+    r.readAsBinaryString(f);
+  });
 };
 
-export const groupByPrefix = (strings) => {
+const groupByPrefix = (strings) => {
   return ({
     group1: ['word1', 'word2'],
     group2: ['word3', 'word4', 'word5'],
